05902c9ffad52436ed4ef625b65537cdb2a232c8
sfreiman <sfreiman@SFREIMAN-AR.ar.oracle.com>
2014-11-29 12:15:24 -0300

    Semantic: Solve and validate arismetic expr

--- src/main/java/edu/maimonides/multimedia/shapes4learn/analysis/SemanticAnalyzer.java
+++ src/main/java/edu/maimonides/multimedia/shapes4learn/analysis/SemanticAnalyzer.java
@@ -1,23 +1,27 @@
 package edu.maimonides.multimedia.shapes4learn.analysis;
 
+import edu.maimonides.multimedia.shapes4learn.interpreter.ArismeticSolverVisitor;
 import edu.maimonides.multimedia.shapes4learn.model.AST;
 
 /**
  * This class is responsible for the third part of the interpreter: semantic
  * analysis. It will be implemented by the students to perform the proper
  * operations.
- * 
+ *
  * @author Matias Giorgio
- * 
+ *
  */
 public class SemanticAnalyzer {
 
-	public SemanticAnalyzer() {
-	}
+    public SemanticAnalyzer() {
+    }
 
-	public AST analyze(AST ast) throws SemanticException {
-		// TODO Implement.
-            
-		return ast;
-	}
+    public AST analyze(AST myAST) throws SemanticException, LexicalException {
+        AST resultAST=myAST;
+        ArismeticSolverVisitor ASV=new ArismeticSolverVisitor();
+        
+        resultAST.preOrder(ASV);
+
+        return resultAST;
+    }
 }
